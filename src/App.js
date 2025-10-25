1fc116e138f28f2d64172821e6af1938c3a34f2c
vdharmendra <dhiru860.com>
2025-10-25 17:07:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if(running){
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if(!running){
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center justify-center py-8'>
+      <h1 className='text-2xl font-semibold pb-2'>StopWatch </h1>
+      <div className='text-xl font-semibold'>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+        {
+          running ? (
+            <button className='border rounded-lg py-1 px-3.5' onClick={() => {setRunning(false)}}>Stop</button>
+          ) : (
+            <button className='border rounded-lg py-1 px-3' onClick={() => {setRunning(true)}}>Start</button>
+          )
+        }
+        <button className='border rounded-lg py-1 px-2.5' onClick={() => {setTime(0) }}>Reset</button>
+      </div>
     </div>
   );
 }
